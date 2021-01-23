e906adc4af7cee2c2760ff41d51ffc9caa2e4958
Susanto <51540113+nerdyspook@users.noreply.github.com>
2021-01-23 12:34:05 +0530

    Add files via upload

---
+++ responses.py
@@ -0,0 +1,8 @@
+response_a = "The {} has "
+response_b = "We have a selection of meals to go along with the {}, including mashed potatoes and steamed vegatables."
+response_c = "{} includes Red chillies, so it is a bit spicy!"
+response_d = "Awesome!\nWe have 3 options for {}\n Grilled \n Butter \n Barbecue \n"
+blank_spot = ["food", "Chicken", "Paneer", "Meat","Chana"]
+
+
+responses = [response_a, response_b, response_c, response_d]
